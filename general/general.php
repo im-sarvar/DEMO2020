e33b7e7bcee712edac9e52bd66d2e794ba358be4
im-sarvar <mohammedsarvar1995@gmail.com>
2021-05-27 17:38:47 +0530

    update

---
+++ general/general.php
@@ -0,0 +1,10 @@
+<?php
+// Your code here!
+$arr = ['1'=>'one','2'=>'two','3'=>'three','4'=>'four'];
+$implode = implode($arr);
+print_r($implode);
+
+/*$arrkey = array_keys($arr);*/
+echo array_key_last ( $arr );
+
+?>
