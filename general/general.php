0401ac67279155759a2523072ad22594b5575d6a
im-sarvar <mohammedsarvar1995@gmail.com>
2021-05-28 11:26:09 +0530

    updated

--- general/general.php
+++ general/general.php
@@ -1,10 +1,15 @@
 <?php
 // Your code here!
 $arr = ['1'=>'one','2'=>'two','3'=>'three','4'=>'four'];
+
+
 $implode = implode($arr);
 print_r($implode);
 
 /*$arrkey = array_keys($arr);*/
-echo array_key_last ( $arr );
+//echo array_key_last ( $arr );
 
+$arr1 = ['1','2','3','4'];
+$arr2 = ['1','2','3','4'];
+print_r(array_combine($arr, $arr2));
 ?>
